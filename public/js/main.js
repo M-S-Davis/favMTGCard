2fd6257af3b92ed7ff7760941800a4d09c5c3d91
Mike Davis <Mecro@live.ca>
2022-06-16 17:19:41 -0400

    styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 document.querySelector("#clickMe").addEventListener("click", getCard);
 
-async function getCard(e) {
-  // e.preventDefault();
+async function getCard() {
   const cardName = document.querySelector("#cardName").value;
   const userName = document.querySelector("#userName").value;
 
@@ -25,3 +24,27 @@ async function getCard(e) {
   //Card converted mana cost
   document.querySelector(".cmc").textContent = data.cards[0].cmc;
 }
+
+async function testCard() {
+  const res = await fetch(
+    `https://api.magicthegathering.io/v1/cards?name=shock`
+  );
+  const data = await res.json();
+
+  console.log(data);
+
+  console.log(data.cards[0].name);
+
+  //Card image
+  document.querySelector(".card-image").src = data.cards[0].imageUrl;
+  //Card Title
+  document.querySelector(".card-title").textContent = data.cards[0].name;
+  //User who added the card
+  document.querySelector(".user-name").textContent = "userName";
+  //Card text
+  document.querySelector(".card-text").textContent = data.cards[0].text;
+  //Card converted mana cost
+  document.querySelector(".cmc").textContent = data.cards[0].cmc;
+}
+
+testCard();
